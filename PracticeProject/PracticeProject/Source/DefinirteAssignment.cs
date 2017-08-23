dfeffa5262c8e35335c4297c9faabc920c902142
ShashankGargeshwari <shashank.gargeshwari@gmail.com>
2017-08-24 00:01:46 +0530

    Patchwork to get the project working

---
+++ PracticeProject/PracticeProject/Source/DefinirteAssignment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace PracticeProject
+{
+	public class DefinirteAssignment
+	{
+		static void iMain()
+		{
+			int x = 10;
+			int y;
+			Console.WriteLine(x ); //Add +y in brackets for demo
+			y = x;
+		}
+		public DefinirteAssignment()
+		{
+			 
+		}
+	}
+}
---
+++ PracticeProject/PracticeProject/Source/Enumeration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace PracticeProject
+{
+	public class Enumeration
+	{
+		enum Marks
+		{
+			Abyssmal = 0,
+			Useless = 25,
+			JustPass = 40,
+			MidPack = 60,
+			SomeWhatOK = 75,
+			BelowExpectation = 90,
+			CanDoBetter = 98,
+			Ideal = 102,
+		}
+		static void iMain()
+		{
+			Console.WriteLine("Why have you got {0}??? Look at your cousin, She got {1} and still she is crying!" , (int)Marks.SomeWhatOK , (int)Marks.CanDoBetter);
+		}
+		public Enumeration()
+		{
+		}
+	}
+}
